49b33d408b37bc05922b25305d7122c628d8d804
ireullin <cheinheng.lin@uitox.com>
2016-04-08 14:17:45 +0800

    Replaced tab to 4 spaces

--- nail/Json.h
+++ nail/Json.h
@@ -7,277 +7,277 @@ namespace nail{
 class JsonNode
 {
 private:
-	typedef std::map<std::string, JsonNode*>	JSONMap;
-	typedef std::pair<std::string, JsonNode*>	JSONPair;
-	typedef std::vector<JsonNode*>				JSONArray;
+    typedef std::map<std::string, JsonNode*>  JSONMap;
+    typedef std::pair<std::string, JsonNode*> JSONPair;
+    typedef std::vector<JsonNode*>            JSONArray;
 
-	JsonNode(const JsonNode&);
-	JsonNode& operator= (const JsonNode&);
+    JsonNode(const JsonNode&);
+    JsonNode& operator= (const JsonNode&);
 
-	static const char SYMBOL;
+    static const char SYMBOL;
 
-	JSONMap*	m_map;
-	JSONArray*	m_array;
-	std::string m_content;
-	
-
-	std::string find(const std::string& data, size_t& offset, const char endSign)
-	{TRACE_THIS_FUNCTION(ONLY_SHOW)
-
-		std::stack<char> _stack;
-		
-		size_t _length = std::string::npos;
-		size_t _total = data.size()-offset;
-
-		for(size_t i=0; i<_total; i++ )
-		{
-			char _c = data[offset+i];
-
-			if( _c=='{' || _c=='[' )
-				_stack.push( _c );
-
-			if( _c=='}' || _c==']' )
-			{
-				if( _c != _stack.top()+2 )
-				{
-					throw NAIL_EXPCEPTION_1("JSON format error");
-				}
-				
-				_stack.pop();
-			}
+    JSONMap*    m_map;
+    JSONArray*  m_array;
+    std::string m_content;
 
 
-			if(_stack.empty())
-			{
-				if( _c==endSign )
-				{
-					_length = i;
-					break;
-				}
-			}
-		}
+    std::string find(const std::string& data, size_t& offset, const char endSign)
+    {TRACE_THIS_FUNCTION(ONLY_SHOW)
 
-		std::string _rc = data.substr(offset, _length);
+        std::stack<char> _stack;
 
-		if(_length != std::string::npos)
-			offset += _length;
-		else
-			offset = std::string::npos;
+        size_t _length = std::string::npos;
+        size_t _total = data.size()-offset;
 
-		size_t _b = _rc.find_first_not_of(std::string(" "));
-		size_t _e = _rc.find_last_not_of(std::string(" "));
+        for(size_t i=0; i<_total; i++ )
+        {
+            char _c = data[offset+i];
 
-		if(_b!=std::string::npos && _e!=std::string::npos)
-			return _rc.substr(_b, _e-_b+1);
+            if( _c=='{' || _c=='[' )
+                _stack.push( _c );
 
-		if(_b==std::string::npos && _e!=std::string::npos)
-			return _rc.substr(0, _e+1);
+            if( _c=='}' || _c==']' )
+            {
+                if( _c != _stack.top()+2 )
+                {
+                    throw NAIL_EXPCEPTION_1("JSON format error");
+                }
 
-		if(_b!=std::string::npos && _e==std::string::npos)
-			return _rc.substr(_b);
-
-		throw NAIL_EXPCEPTION_1("JSON format error");
-		return "";
-	}
+                _stack.pop();
+            }
 
 
-	void hashType(const std::string& content)
-	{TRACE_THIS_FUNCTION(ONLY_SHOW)
+            if(_stack.empty())
+            {
+                if( _c==endSign )
+                {
+                    _length = i;
+                    break;
+                }
+            }
+        }
 
-		m_map = new JSONMap();
+        std::string _rc = data.substr(offset, _length);
 
-		size_t _offset = -1;
-		do
-		{
-			_offset++;
-			std::string _key = find(content, _offset, ':');
-			if( _key[0]!=JsonNode::SYMBOL || _key[ _key.size()-1 ]!=JsonNode::SYMBOL )
-			{
-				throw NAIL_EXPCEPTION_1("JSON format error");
-			}
+        if(_length != std::string::npos)
+            offset += _length;
+        else
+            offset = std::string::npos;
 
-			_offset++;
-			std::string _val = find(content, _offset, ',');
-			
-			m_map->insert(
-				JSONPair(
-					_key.substr(1,_key.size()-2),
-					 new JsonNode( _val )
-					)
-				);
+        size_t _b = _rc.find_first_not_of(std::string(" "));
+        size_t _e = _rc.find_last_not_of(std::string(" "));
 
-		}while(_offset!=std::string::npos);
-	}
+        if(_b!=std::string::npos && _e!=std::string::npos)
+            return _rc.substr(_b, _e-_b+1);
+
+        if(_b==std::string::npos && _e!=std::string::npos)
+            return _rc.substr(0, _e+1);
+
+        if(_b!=std::string::npos && _e==std::string::npos)
+            return _rc.substr(_b);
+
+        throw NAIL_EXPCEPTION_1("JSON format error");
+        return "";
+    }
 
 
-	void arrayType(const std::string& content)
-	{TRACE_THIS_FUNCTION(ONLY_SHOW)
+    void hashType(const std::string& content)
+    {TRACE_THIS_FUNCTION(ONLY_SHOW)
 
-		m_array = new JSONArray();
+        m_map = new JSONMap();
 
-		size_t _offset = -1;
-		do
-		{
-			_offset++;
-			std::string _val = find(content, _offset, ',');
-			m_array->push_back( new JsonNode( _val ) );
+        size_t _offset = -1;
+        do
+        {
+            _offset++;
+            std::string _key = find(content, _offset, ':');
+            if( _key[0]!=JsonNode::SYMBOL || _key[ _key.size()-1 ]!=JsonNode::SYMBOL )
+            {
+                throw NAIL_EXPCEPTION_1("JSON format error");
+            }
 
-		}while(_offset!=std::string::npos);
-	}
+            _offset++;
+            std::string _val = find(content, _offset, ',');
+
+            m_map->insert(
+                JSONPair(
+                    _key.substr(1,_key.size()-2),
+                     new JsonNode( _val )
+                    )
+                );
+
+        }while(_offset!=std::string::npos);
+    }
+
+
+    void arrayType(const std::string& content)
+    {TRACE_THIS_FUNCTION(ONLY_SHOW)
+
+        m_array = new JSONArray();
+
+        size_t _offset = -1;
+        do
+        {
+            _offset++;
+            std::string _val = find(content, _offset, ',');
+            m_array->push_back( new JsonNode( _val ) );
+
+        }while(_offset!=std::string::npos);
+    }
 
 
 public:
 
-	JsonNode(const std::string& content)
-		:m_map(NULL),m_array(NULL)
-	{TRACE_THIS_FUNCTION(ONLY_SHOW)
+    JsonNode(const std::string& content)
+        :m_map(NULL),m_array(NULL)
+    {TRACE_THIS_FUNCTION(ONLY_SHOW)
 
-		// to do:
-		// check the rule
-		if(content[0]=='{')
-		{
-			if(content[content.size()-1]=='}')
-				hashType( content.substr(1, content.size()-2) );
-			else
-			{	
-				throw NAIL_EXPCEPTION_1("JSON format error");
-			}
-		}
-		else if(content[0]=='[')
-		{
-			if(content[content.size()-1]==']')
-				arrayType( content.substr(1, content.size()-2) );
-			else
-			{
-				throw NAIL_EXPCEPTION_1("JSON format error");
-			}
-		}
-		else
-		{
-			if(content[0]=='"' && content[ content.size()-1 ]=='"')
-				m_content = content.substr(1, content.size()-2);
-			else if(content[0]!='"' && content[ content.size()-1 ]!='"')
-				m_content = content;
-			else
-			{
-				throw NAIL_EXPCEPTION_1("JSON format error");
-			}
-		}
-	}
-
-
-	virtual ~JsonNode()
-	{
-
-		if(m_map!=NULL)
-		{
-			for(JSONMap::iterator it=m_map->begin(); it!=m_map->end(); ++it)
-			{
-				JsonNode* _node = it->second;
-    			delete _node;
-    		}
-
-    		delete m_map;
-		}
-
-		if(m_array!=NULL)
-		{
-			for(JSONArray::iterator it=m_array->begin(); it!=m_array->end(); ++it)
-			{
-    			JsonNode* _node = (*it);
-    			delete _node;
-    		}
-
-    		delete m_array;
-		}
-	}
+        // to do:
+        // check the rule
+        if(content[0]=='{')
+        {
+            if(content[content.size()-1]=='}')
+                hashType( content.substr(1, content.size()-2) );
+            else
+            {
+                throw NAIL_EXPCEPTION_1("JSON format error");
+            }
+        }
+        else if(content[0]=='[')
+        {
+            if(content[content.size()-1]==']')
+                arrayType( content.substr(1, content.size()-2) );
+            else
+            {
+                throw NAIL_EXPCEPTION_1("JSON format error");
+            }
+        }
+        else
+        {
+            if(content[0]=='"' && content[ content.size()-1 ]=='"')
+                m_content = content.substr(1, content.size()-2);
+            else if(content[0]!='"' && content[ content.size()-1 ]!='"')
+                m_content = content;
+            else
+            {
+                throw NAIL_EXPCEPTION_1("JSON format error");
+            }
+        }
+    }
 
 
-	JsonNode& operator[](const std::string& key)
-	{
-		return (*(*m_map)[key]);
-	}
+    virtual ~JsonNode()
+    {
+
+        if(m_map!=NULL)
+        {
+            for(JSONMap::iterator it=m_map->begin(); it!=m_map->end(); ++it)
+            {
+                JsonNode* _node = it->second;
+                delete _node;
+            }
+
+            delete m_map;
+        }
+
+        if(m_array!=NULL)
+        {
+            for(JSONArray::iterator it=m_array->begin(); it!=m_array->end(); ++it)
+            {
+                JsonNode* _node = (*it);
+                delete _node;
+            }
+
+            delete m_array;
+        }
+    }
 
 
-	JsonNode& operator[](int key)
-	{
-		return (*(*m_array)[key]);
-	}
+    JsonNode& operator[](const std::string& key)
+    {
+        return (*(*m_map)[key]);
+    }
 
 
-	std::string toString()
-	{
-		if( !m_content.empty() )
-		{
-			std::string _rc = "\"" + m_content + "\"";
-			return _rc;
-		}
-		
-		
-		if(m_map!=NULL)
-		{
-			std::stringstream _ss;
-			_ss << "{";
-			for(JSONMap::iterator it=m_map->begin(); it!=m_map->end(); ++it)
-			{
-				JsonNode* _node = it->second;
-    			_ss << '"' << it->first << "\":" << _node->toString() << ",";
-    		}
-
-    		std::string _rc = _ss.str();
-    		_rc[ _rc.size()-1 ] = '}';
-    		return _rc;
-		}
+    JsonNode& operator[](int key)
+    {
+        return (*(*m_array)[key]);
+    }
 
 
-		if(m_array!=NULL)
-		{
-			std::stringstream _ss;
-			_ss << "[";
-			for(JSONArray::iterator it=m_array->begin(); it!=m_array->end(); ++it)
-			{
-				JsonNode* _node = (*it);
-    			_ss << _node->toString() << ",";
-    		}
-
-    		std::string _rc = _ss.str();
-    		_rc[ _rc.size()-1 ] = ']';
-    		return _rc;
-		}
-
-		return "";
-	}
+    std::string toString()
+    {
+        if( !m_content.empty() )
+        {
+            std::string _rc = "\"" + m_content + "\"";
+            return _rc;
+        }
 
 
-	double toDouble()
-	{
-		if(m_array!=NULL || m_map!=NULL)
-		{
-			throw NAIL_EXPCEPTION_1("this node is an object");
-		}
+        if(m_map!=NULL)
+        {
+            std::stringstream _ss;
+            _ss << "{";
+            for(JSONMap::iterator it=m_map->begin(); it!=m_map->end(); ++it)
+            {
+                JsonNode* _node = it->second;
+                _ss << '"' << it->first << "\":" << _node->toString() << ",";
+            }
 
-		return atof(m_content.c_str());
-	}
-
-
-	int toInt()
-	{
-		if(m_array!=NULL || m_map!=NULL)
-		{
-			throw NAIL_EXPCEPTION_1("this node is an object");
-		}
-
-		return atoi(m_content.c_str());
-	}
+            std::string _rc = _ss.str();
+            _rc[ _rc.size()-1 ] = '}';
+            return _rc;
+        }
 
 
-	template<typename T>
-	void operator=(const T& val)
-	{
-		std::stringstream _ss;
-		_ss << val;
-		m_content = _ss.str();
-	}
+        if(m_array!=NULL)
+        {
+            std::stringstream _ss;
+            _ss << "[";
+            for(JSONArray::iterator it=m_array->begin(); it!=m_array->end(); ++it)
+            {
+                JsonNode* _node = (*it);
+                _ss << _node->toString() << ",";
+            }
+
+            std::string _rc = _ss.str();
+            _rc[ _rc.size()-1 ] = ']';
+            return _rc;
+        }
+
+        return "";
+    }
+
+
+    double toDouble()
+    {
+        if(m_array!=NULL || m_map!=NULL)
+        {
+            throw NAIL_EXPCEPTION_1("this node is an object");
+        }
+
+        return atof(m_content.c_str());
+    }
+
+
+    int toInt()
+    {
+        if(m_array!=NULL || m_map!=NULL)
+        {
+            throw NAIL_EXPCEPTION_1("this node is an object");
+        }
+
+        return atoi(m_content.c_str());
+    }
+
+
+    template<typename T>
+    void operator=(const T& val)
+    {
+        std::stringstream _ss;
+        _ss << val;
+        m_content = _ss.str();
+    }
 };
 
 const char JsonNode::SYMBOL = '"';
